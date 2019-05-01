2fe277bdfd2db20d1625b248115c5efb92e2875b
Guillaume Leclerc <guill.bout@gmail.com>
2019-05-02 03:16:16 +0800

    Add docset file creation, copy docs into it, and Info.plist

---
+++ app/Commands/PackageCommand.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use LaravelZero\Framework\Commands\Command;
+
+class PackageCommand extends Command
+{
+    const DOCS = [
+        'tailwindcss' => [
+            'code' => 'tailwindcss',
+            'name' => 'Tailwind CSS',
+            'url' => 'next.tailwindcss.com'
+        ]
+    ];
+
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'package {docs}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Package the docs specified as argument as a Dash docset file.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $doc = $this->argument('docs');
+
+        if (! $this->isValid($doc)) {
+            $this->comment('Only the tailwindcss doc is currently available.');
+
+            return 1;
+        }
+
+        $this->task('Package creation started', function () {
+            return true;
+        });
+
+        $this->package(self::DOCS[$doc]);
+
+        $this->task('Package creation finished', function () {
+            return true;
+        });
+    }
+
+    protected function isValid($doc)
+    {
+        return array_key_exists($doc, self::DOCS);
+    }
+
+    protected function package($doc)
+    {
+        $this->createDocsetFile($doc);
+
+        $this->copyDocFiles($doc);
+
+        $this->createInfoPlist($doc);
+    }
+
+    protected function createDocsetFile($doc)
+    {
+        Storage::makeDirectory("{$doc['code']}/{$doc['code']}.docset/Contents/Resources/Documents");
+    }
+
+    protected function copyDocFiles($doc)
+    {
+        File::copyDirectory(
+            "storage/{$doc['code']}/{$doc['url']}",
+            "storage/{$doc['code']}/{$doc['code']}.docset/Contents/Resources/Documents"
+        );
+    }
+
+    protected function createInfoPlist($doc)
+    {
+        $infoPlist = <<<EOT
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>CFBundleIdentifier</key>
+    <string>{$doc['code']}</string>
+    <key>CFBundleName</key>
+    <string>{$doc['name']}</string>
+    <key>DocSetPlatformFamily</key>
+    <string>{$doc['code']}</string>
+    <key>isDashDocset</key>
+    <true/>
+</dict>
+</plist>
+EOT;
+
+        Storage::put("{$doc['code']}/{$doc['code']}.docset/Contents/Info.plist", $infoPlist);
+    }
+}
---
+++ tests/Feature/PackageCommandTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Commands\PackageCommand;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Console\Exception\RuntimeException;
+
+class PackageCommandTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Storage::deleteDirectory('tailwindcss/tailwindcss.docset');
+
+        if (! Storage::exists('tailwindcss/next.tailwindcss.com')) {
+            Storage::put(
+                'tailwindcss/next.tailwindcss.com/what.txt',
+                'If you see this it means my package creation fucked up.'
+            );
+        }
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+
+        /**
+         * Crashes for now. Don't know yet why.
+         */
+        // Storage::delete('tailwindcss/next.tailwindcss.com/file.txt');
+        exec('rm -Rf storage/tailwindcss/tailwindcss.docset');
+    }
+
+    /** @test */
+    public function it_requires_a_docset_name_as_argument()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Not enough arguments (missing: "docs").');
+
+        $this->artisan('package');
+    }
+
+    /** @test */
+    public function it_currently_only_allows_tailwindcss_as_argument()
+    {
+        $this->artisan('package tailwindcss')
+            ->assertExitCode(0);
+
+        $this->artisan('package nottailwindcss')
+            ->expectsOutput('Only the tailwindcss doc is currently available.')
+            ->assertExitCode(1);
+    }
+
+    /** @test */
+    public function it_creates_the_dash_docset_package()
+    {
+        $this->artisan('package tailwindcss');
+
+        $this->assertFileExists('storage/tailwindcss/tailwindcss.docset');
+    }
+
+    /** @test */
+    public function it_creates_the_info_plist_file()
+    {
+        $this->artisan('package tailwindcss');
+
+        $this->assertFileExists(
+            'storage/tailwindcss/tailwindcss.docset/Contents/Info.plist'
+        );
+    }
+}
