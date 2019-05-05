a0ac0f61174869fee400c53c072d28530a95a2ce
Guillaume Leclerc <guill.bout@gmail.com>
2019-05-05 15:46:05 +0800

    Improve package test by testing css before and after package created

--- tests/Feature/Commands/PackageTest.php
+++ tests/Feature/Commands/PackageTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Commands;
 
 use Tests\TestCase;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Console\Exception\RuntimeException;
 
@@ -19,12 +20,12 @@ class PackageTest extends TestCase
     }
 
     /** @test */
-    public function it_currently_only_allows_tailwindcss_as_argument()
+    public function the_command_returns_an_info_message_if_the_docset_is_not_supported()
     {
-        $this->artisan('package tailwindcss')
+        $this->artisan('package dummy')
             ->assertExitCode(0);
 
-        $this->artisan('package nottailwindcss')
+        $this->artisan('package unsupported')
             ->expectsOutput('The doc requested does not seem to be supported.')
             ->assertExitCode(1);
     }
@@ -32,14 +33,14 @@ class PackageTest extends TestCase
     /** @test */
     public function it_creates_the_dash_docset_package()
     {
-        $this->assertFileExists('storage/tailwindcss/tailwindcss.docset');
+        $this->assertFileExists('storage/dummy/dummy.docset');
     }
 
     /** @test */
     public function it_creates_the_info_plist_file()
     {
         $this->assertFileExists(
-            'storage/tailwindcss/tailwindcss.docset/Contents/Info.plist'
+            'storage/dummy/dummy.docset/Contents/Info.plist'
         );
     }
 
@@ -47,13 +48,18 @@ class PackageTest extends TestCase
     public function it_creates_the_sqlite_index()
     {
         $this->assertFileExists(
-            'storage/tailwindcss/tailwindcss.docset/Contents/Resources/docSet.dsidx'
+            'storage/dummy/dummy.docset/Contents/Resources/docSet.dsidx'
         );
     }
 
     /** @test */
     public function it_fills_up_the_sqlite_index()
     {
+        Config::set(
+            'database.connections.sqlite.database',
+            'storage/dummy/dummy.docset/Contents/Resources/docSet.dsidx'
+        );
+
         $indexes = DB::table('searchIndex')->get();
 
         $this->assertNotEmpty($indexes);
@@ -62,19 +68,16 @@ class PackageTest extends TestCase
     /** @test */
     public function it_formats_the_doc_especially_for_dash()
     {
-        $this->assertStringNotContainsString(
-            'id="sidebar-open"',
-            Storage::get('tailwindcss/tailwindcss.docset/Contents/Resources/Documents/index.html')
+        $navBar = 'class="nav-item';
+
+        $this->assertStringContainsString(
+            $navBar,
+            Storage::get('dummy/docs/sleeplessmind.info/index.html')
         );
 
         $this->assertStringNotContainsString(
-            'id="sidebar"',
-            Storage::get('tailwindcss/tailwindcss.docset/Contents/Resources/Documents/index.html')
-        );
-
-        $this->assertStringNotContainsString(
-            'hidden xl:text-sm',
-            Storage::get('tailwindcss/tailwindcss.docset/Contents/Resources/Documents/index.html')
+            $navBar,
+            Storage::get('dummy/dummy.docset/Contents/Resources/Documents/index.html')
         );
     }
 
@@ -82,11 +85,11 @@ class PackageTest extends TestCase
     public function it_generates_icons_for_the_docset()
     {
         $this->assertFileExists(
-            'storage/tailwindcss/tailwindcss.docset/icon.png'
+            'storage/dummy/dummy.docset/icon.png'
         );
 
         $this->assertFileExists(
-            'storage/tailwindcss/tailwindcss.docset/icon@2x.png'
+            'storage/dummy/dummy.docset/icon@2x.png'
         );
     }
 }
