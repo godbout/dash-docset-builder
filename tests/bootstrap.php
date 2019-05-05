107127293be9e1e8b3aedbe92adc34ff1fc64a5c
Guill Lo <guill.bout@gmail.com>
2019-05-05 13:35:46 +0800

    Make tests not download docs if already downloaded

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,13 @@ include 'vendor/autoload.php';
 
 echo <<<EOT
 
-\e[1;33m### Grabbing and packaging a dummy docset once before running all the tests ###
+\e[1;33m### Grabbing (if needed) and packaging the dummy docset before running the tests ###
 
 
 EOT;
 
-print passthru('php dash-docset grab dummy --ansi');
+if (! file_exists(__DIR__ . '/../storage/dummy')) {
+    print passthru('php dash-docset grab dummy --ansi');
+}
+
 print passthru('php dash-docset package dummy --ansi');
