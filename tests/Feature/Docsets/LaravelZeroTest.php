3daec93c2d6f333147857118caedfd496102e93a
Guillaume Leclerc <guill.bout@gmail.com>
2019-05-08 16:58:46 +0800

    Fix typo in laravel zero test class

--- tests/Feature/Docsets/LaravelZeroTest.php
+++ tests/Feature/Docsets/LaravelZeroTest.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
 
 /** @group laravel-zero */
-class LaravelZerlTest extends TestCase
+class LaravelZeroTest extends TestCase
 {
     public function setUp(): void
     {
