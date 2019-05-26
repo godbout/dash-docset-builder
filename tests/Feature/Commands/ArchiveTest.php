379d7e9b0c6872050620f2d7f0a296b6efd73e06
Guillaume Leclerc <guill.bout@gmail.com>
2019-05-26 17:38:14 +0800

    Add archive test

---
+++ tests/Feature/Commands/ArchiveTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Commands;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Console\Exception\RuntimeException;
+
+class ArchiveTest extends TestCase
+{
+    /** @test */
+    public function the_command_requires_a_docset_name_as_argument()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Not enough arguments (missing:');
+
+        $this->artisan('archive');
+    }
+
+    /** @test */
+    public function the_command_returns_an_info_message_if_the_docset_is_not_supported()
+    {
+        $this->artisan('archive unsupported')
+            ->expectsOutput('The doc requested does not seem to be supported.')
+            ->assertExitCode(1);
+    }
+
+    /** @test */
+    public function the_command_generates_an_archive_file()
+    {
+        $this->artisan('archive dummy');
+
+        $this->assertTrue(Storage::exists('dummy/dummy.tgz'));
+    }
+}
